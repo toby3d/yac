aa50bb18be2f6b004de8229de68da95003de01fc
Maxim Lebedev <git@toby3d.ru>
2017-12-06 20:17:41 +0500

    :sparkles: Sorting current Colors array by light or dark theme

--- sort.go
+++ sort.go
@@ -3,11 +3,28 @@ package yac
 import "sort"
 
 // Sort is a helper method which sort array of key Colors by lightness. Returned
-// Colors array of sorted colors from higher to lower lightness.
+// Colors array with sorted key colors: from the lightest to the darkest colors,
+// if the current array is a light theme and vice versa, if it's dark.
 func (c Colors) Sort() Colors {
-	sort.SliceStable(c, func(i, j int) bool {
-		return getLightness(c[i]) > getLightness(c[j])
-	})
+	var light, dark float64
+	for i := range c {
+		shade := getLightness(c[i])
+		if getLightness(c[i]) >= 50 {
+			light += shade
+		} else {
+			dark += shade
+		}
+	}
+
+	if light >= dark {
+		sort.SliceStable(c, func(i, j int) bool {
+			return getLightness(c[i]) > getLightness(c[j])
+		})
+	} else {
+		sort.SliceStable(c, func(i, j int) bool {
+			return getLightness(c[i]) < getLightness(c[j])
+		})
+	}
 
 	return c
 }
