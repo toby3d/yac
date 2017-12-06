e5d4604cc4bf832608353716af8ca25a57294122
Maxim Lebedev <git@toby3d.ru>
2017-12-06 19:56:05 +0500

    :sparkles: Added Prune helper for returning unique colors

---
+++ prune.go
@@ -0,0 +1,22 @@
+package yac
+
+import "image/color"
+
+// Prune helper check Colors array on duplicated items. Returned Colors array
+// with unique color.Color items.
+func (c Colors) Prune() Colors {
+	sorted := map[color.Color]bool{}
+
+	// Create a map of all unique elements.
+	for i := range c {
+		sorted[c[i]] = true
+	}
+
+	// Place all keys from the map into a slice.
+	unique := Colors{}
+	for key := range sorted {
+		unique = append(unique, key)
+	}
+
+	return unique
+}
