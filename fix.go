b9bec995bb0ab278db4ffac11c72fcacd3e155ad
Maxim Lebedev <toby3d@yandex.com>
2017-12-07 18:18:09 +0500

    :hammer: Refactor analytics formula in Fix method

--- fix.go
+++ fix.go
@@ -11,63 +11,68 @@ func Fix(c []color.Color) []color.Color {
 	btnH, btnS, btnL := colorful.MakeColor(c[1]).Hsl()
 	txtH, txtS, txtL := colorful.MakeColor(c[3]).Hsl()
 
-	bgVSbtn := (btnL*100 - bgL*100)
-	bgVStxt := (txtL*100 - bgL*100)
-
-	if bgVSbtn < 20 {
-		if bgL*100 > 50 {
-			if bgL+((20-bgVSbtn)/2/100) < 1 {
-				c[0] = colorful.MakeColor(colorful.Hsl(bgH, bgS, bgL+((20-bgVSbtn)/2/100)))
-			} else {
-				c[0] = colorful.MakeColor(colorful.Hsl(bgH, bgS, 1))
-			}
-
-			if btnL-((20-bgVSbtn)/2/100) > 0 {
-				c[1] = colorful.MakeColor(colorful.Hsl(btnH, btnS, btnL-((20-bgVSbtn)/2/100)))
-			} else {
-				c[0] = colorful.MakeColor(colorful.Hsl(btnH, btnS, 0))
-			}
-		} else {
-			if bgL-((20-bgVSbtn)/2/100) > 0 {
-				c[0] = colorful.MakeColor(colorful.Hsl(bgH, bgS, bgL-((20-bgVSbtn)/2/100)))
-			} else {
-				c[0] = colorful.MakeColor(colorful.Hsl(bgH, bgS, 0))
-			}
-
-			if btnL+((20-bgVSbtn)/2/100) < 1 {
-				c[1] = colorful.MakeColor(colorful.Hsl(btnH, btnS, btnL+((20-bgVSbtn)/2/100)))
-			} else {
-				c[0] = colorful.MakeColor(colorful.Hsl(btnH, btnS, 1))
-			}
-		}
+	var bgVSbtn, bgVStxt float64
+	if bgL >= 0.5 {
+		bgVSbtn = bgL - btnL
+		bgVStxt = bgL - txtL
+	} else {
+		bgVSbtn = btnL - bgL
+		bgVStxt = txtL - bgL
 	}
 
-	if bgVStxt < 60 {
-		if bgL*100 > 50 {
-			if bgL+((60-bgVStxt)/2/100) < 1 {
-				c[0] = colorful.MakeColor(colorful.Hsl(bgH, bgS, bgL+((60-bgVStxt)/2/100)))
-			} else {
-				c[0] = colorful.MakeColor(colorful.Hsl(bgH, bgS, 1))
-			}
-
-			if txtL-((60-bgVStxt)/2/100) > 0 {
-				c[1] = colorful.MakeColor(colorful.Hsl(txtH, txtS, txtL-((60-bgVStxt)/2/100)))
-			} else {
-				c[0] = colorful.MakeColor(colorful.Hsl(txtH, txtS, 0))
-			}
+	if bgVSbtn < 0.2 {
+		if bgL >= 0.5 {
+			bgL += (0.2 - bgVSbtn) / 2
+			btnL -= (0.2 - bgVSbtn) / 2
 		} else {
-			if bgL-((60-bgVStxt)/2/100) > 0 {
-				c[0] = colorful.MakeColor(colorful.Hsl(bgH, bgS, bgL-((60-bgVStxt)/2/100)))
-			} else {
-				c[0] = colorful.MakeColor(colorful.Hsl(bgH, bgS, 0))
-			}
-
-			if txtL+((60-bgVStxt)/2/100) < 1 {
-				c[1] = colorful.MakeColor(colorful.Hsl(txtH, txtS, txtL+((60-bgVStxt)/2/100)))
-			} else {
-				c[0] = colorful.MakeColor(colorful.Hsl(txtH, txtS, 1))
-			}
+			bgL -= (0.2 - bgVSbtn) / 2
+			btnL += (0.2 - bgVSbtn) / 2
 		}
+
+		if bgL <= 0 {
+			bgL = 0
+		} else if bgL >= 1 {
+			bgL = 1
+		}
+
+		if btnL <= 0 {
+			btnL = 0
+		} else if btnL >= 1 {
+			btnL = 1
+		}
+
+		bgNewR, bgNewG, bgNewB := colorful.Hsl(bgH, bgS, bgL).RGB255()
+		btnNewR, btnNewG, btnNewB := colorful.Hsl(btnH, btnS, btnL).RGB255()
+		c[0] = color.RGBA{R: bgNewR, G: bgNewG, B: bgNewB}
+		c[1] = color.RGBA{R: btnNewR, G: btnNewG, B: btnNewB}
+	}
+
+	if bgVStxt < 0.6 {
+		if bgL >= 0.5 {
+			bgL += (0.6 - bgVStxt) / 2
+			txtL -= (0.6 - bgVStxt) / 2
+		} else {
+			bgL -= (0.6 - bgVStxt) / 2
+			txtL += (0.6 - bgVStxt) / 2
+		}
+
+		if bgL <= 0 {
+			bgL = 0
+		} else if bgL >= 1 {
+			bgL = 1
+		}
+
+		if txtL <= 0 {
+			txtL = 0
+		} else if txtL >= 1 {
+			txtL = 1
+		}
+
+		bgNewR, bgNewG, bgNewB := colorful.Hsl(bgH, bgS, bgL).RGB255()
+		txtNewR, txtNewG, txtNewB := colorful.Hsl(txtH, txtS, txtL).RGB255()
+
+		c[0] = color.RGBA{R: bgNewR, G: bgNewG, B: bgNewB}
+		c[3] = color.RGBA{R: txtNewR, G: txtNewG, B: txtNewB}
 	}
 
 	return c
